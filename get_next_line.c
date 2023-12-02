39c44b167a2a185ee1cb8e272fc05c15374c2c85
Mustapha Kadri <mustaphakadri@MacBook-Air-de-Mustapha.local>
2023-12-02 21:18:08 +0100

    test to open file and get BUFFER_SIZE

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,36 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 20:25:46 by mkadri            #+#    #+#             */
-/*   Updated: 2023/12/01 20:28:50 by mkadri           ###   ########.fr       */
+/*   Updated: 2023/12/02 21:10:28 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
 
 char	*get_next_line(int fd)
 {
+	static	char *tmp = NULL;
 
+	if(fd < 0 || BUFFER_SIZE <= 0 )
+		return(NULL);
+	printf("%d", BUFFER_SIZE);
+	return("Hello");	
+}
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
+	line = "";
+		line = get_next_line(fd);
+		printf("%s", line);
+	fd = close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,18 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 20:26:04 by mkadri            #+#    #+#             */
-/*   Updated: 2023/12/01 20:28:28 by mkadri           ###   ########.fr       */
+/*   Updated: 2023/12/02 21:15:00 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GNL_H
 # define GNL_H
+
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE = 5
+# endif
+
 
 char	*get_next_line(int fd);
 
